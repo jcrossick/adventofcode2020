6363cf081cc58f34b5110ba2738557fa029ea0e0
Josephine Crossick <jocrossick@gmail.com>
2020-12-04 08:51:09 +0100

    Parsing into objects

--- day4.js
+++ day4.js
@@ -1,7 +1,13 @@
 const { passportData } = require("./day4data");
 
 const parseData = (input) => {
-    return input.split(/\n\n/).map(passport => passport.split(/\ |\n/))
+  return input
+    .split(/\n\n/)
+    .map((passports) => passports.split(/\ |\n/))
+    .map((passport) => passport.reduce((acc, ppData) => {
+        return { ...acc, [ppData.split(":")[0]]: ppData.split(":")[1] };
+      }, {})
+    );
 };
 
 module.exports = { parseData };
