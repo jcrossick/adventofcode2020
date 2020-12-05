605cff7b759094b30ffae67b65d5cbd28e7afcd3
Josephine Crossick <jocrossick@gmail.com>
2020-12-05 08:17:18 +0100

    Part 2 done

--- day5.js
+++ day5.js
@@ -16,6 +16,21 @@ const findHighestSeat = (array, rowCount) =>
     return seatId > acc ? seatId : acc;
   }, 0);
 
+const findMissingSeats = (arr, rowCount) => {
+  const seats = arr.map((row) => calcRow(row, rowCount)[2]);
+  seats.sort((a, b) => a - b);
+  console.log("The seat ids sorted are", seats);
+  const beforeMissingSeat = seats.reduce((acc, seat, i) => {
+    if (seats[i - 1] + 1 != seat) {
+      acc.push(seats[i - 1]);
+    }
+    return acc;
+  }, []);
+  console.log("The seats before the missing seats are", beforeMissingSeat);
+};
+
+console.log("The missing seats are", findMissingSeats(parseData(input), 7));
+
 console.log(
   "The highest seat id in the dataset is",
   findHighestSeat(parseData(input), 7)
