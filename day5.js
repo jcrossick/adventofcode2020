bd818c6cbfa462c0d617d83f1546346e263b8a47
Josephine Crossick <jocrossick@gmail.com>
2020-12-05 07:37:11 +0100

    Day 5 part 1 done

--- day5.js
+++ day5.js
@@ -1,13 +1,24 @@
-const input = require("./day5data");
+const { input } = require("./day5data");
 
-const parseData = (data) => data.split(/\n\n/);
+const parseData = (data) => data.split(/\r?\n/);
 
 const calcRow = (input, rowCount) => {
   const row = input.substring(0, rowCount);
   const seat = input.substring(rowCount);
   const rowNum = parseInt(row.replaceAll(/F/g, 0).replaceAll(/B/g, 1), 2);
-  const seatNum = parseInt(seat.replaceAll(/F/g, 0).replaceAll(/B/g, 1), 2);
-  return [rowNum, seatNum, rowNum*8+seatNum];
+  const seatNum = parseInt(seat.replaceAll(/L/g, 0).replaceAll(/R/g, 1), 2);
+  return [rowNum, seatNum, rowNum * 8 + seatNum];
 };
 
-module.exports = { calcRow };
+const findHighestSeat = (array, rowCount) =>
+  array.reduce((acc, row) => {
+    const seatId = calcRow(row, rowCount)[2];
+    return seatId > acc ? seatId : acc;
+  }, 0);
+
+console.log(
+  "The highest seat id in the dataset is",
+  findHighestSeat(parseData(input), 7)
+);
+
+module.exports = { parseData, calcRow, findHighestSeat };
