53f3f62184629b04d024f727cf5991eadc002ebc
Josephine Crossick <jocrossick@gmail.com>
2020-12-05 07:01:03 +0100

    data input to row, seat calc

--- day5.js
+++ day5.js
@@ -2,8 +2,12 @@ const input = require("./day5data");
 
 const parseData = (data) => data.split(/\n\n/);
 
-const calcRow = (row) => {
-  return parseInt(row.replaceAll(/F/g, 0).replaceAll(/B/g, 1), 2);
+const calcRow = (input, rowCount) => {
+  const row = input.substring(0, rowCount);
+  const seat = input.substring(rowCount);
+  const rowNum = parseInt(row.replaceAll(/F/g, 0).replaceAll(/B/g, 1), 2);
+  const seatNum = parseInt(seat.replaceAll(/F/g, 0).replaceAll(/B/g, 1), 2);
+  return [rowNum, seatNum];
 };
 
 module.exports = { calcRow };
--- day5.test.js
+++ day5.test.js
@@ -2,11 +2,12 @@ const { calcRow } = require("./day5");
 const { test, expect } = require("@jest/globals");
 
 test("it converts the row to a number", () => {
-    //8 rows
-  const testData1 = `FFB`; //1
-  const testData2 = `BFF`; //4
-  const testData3 = `FBF`; //2
-  expect(calcRow(testData1)).toBe(1);
-  expect(calcRow(testData2)).toBe(4);
-  expect(calcRow(testData3)).toBe(2);
+    //8 rows, 4 seats (zero-indexed)
+    const rowCount = 3;
+  const testData1 = `FFBFB`; //1, 1
+  const testData2 = `BFFBF`; //4, 2
+  const testData3 = `FBFBB`; //2, 3
+  expect(calcRow(testData1, 3)).toEqual([1, 1]);
+  expect(calcRow(testData2, 3)).toEqual([4, 2]);
+  expect(calcRow(testData3, 3)).toEqual([2, 3]);
 });
