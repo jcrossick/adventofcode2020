221e35d3b6dfca10054105895a79c81ff13720ac
Josephine Crossick <jocrossick@gmail.com>
2020-12-06 10:13:08 +0100

    Day 6 Part 2

--- day6.js
+++ day6.js
@@ -1,12 +1,37 @@
-const { input } = require('./day6data');
+const { input } = require("./day6data");
 
-const parseData = (input) =>
-  input.replaceAll(/ /g, '')
+const parseDataPart1 = (input) =>
+  input
+    .replaceAll(/ /g, "")
     .split(/\n\n/)
     .map((group) => [...new Set(group.replaceAll(/\n/g, ""))]);
 
-const countAnswers = (answers) => answers.reduce((acc, group) => acc + group.length, 0);
+const countAnswersPart1 = (answers) =>
+  answers.reduce((acc, group) => acc + group.length, 0);
 
-console.log('Total number of answers', countAnswers(parseData(input)));
+const parseDataPart2 = (input) =>
+  input
+    .replaceAll(/ /g, "")
+    .split(/\n\n/)
+    .map((group) => group.split(/\n/));
 
-module.exports = { parseData, countAnswers };
+const matchingAnswersPerGroup = (group) =>
+  group.reduce(
+    (acc, entry) => acc.filter((letter) => entry.indexOf(letter) >= 0),
+    group[0].split("")
+  );
+
+console.log('Total number of answers part 2', parseDataPart2(input).reduce((acc, group) => acc + matchingAnswersPerGroup(group).length, 0));
+
+//6530
+console.log(
+  "Total number of answers part 1",
+  countAnswersPart1(parseDataPart1(input))
+);
+
+module.exports = {
+  parseDataPart1,
+  parseDataPart2,
+  countAnswersPart1,
+  matchingAnswersPerGroup,
+};
--- day6.test.js
+++ day6.test.js
@@ -1,40 +1,14 @@
-const { parseData, countAnswers } = require("./day6");
+const {
+  parseDataPart1,
+  parseDataPart2,
+  countAnswersPart1,
+  matchingAnswersPerGroup,
+} = require("./day6");
 const { test, expect } = require("@jest/globals");
 
-test("it parses the data correctly", () => {
-  const testdata = `ab
-    
-    c
-    d`;
-  expect(parseData(testdata)).toEqual([
-    ["a", "b"],
-    ["c", "d"],
-  ]);
-});
-
-test("it dedupes entries", () => {
-  const testdata = `ab
-    
-    cf
-    df`;
-
-  expect(parseData(testdata)).toEqual([
-    ["a", "b"],
-    ["c", "f", "d"],
-  ]);
-});
-
-test("it counts entries", () => {
-  const testdata4 = `ab
-    
-    a
-    b`;
-  const testdata5 = `ab
-      
-      cf
-      df`;
-
-  const testData38 = `obegcmqadtrhui
+describe("Part 2", () => {
+  test("it parses data correctly", () => {
+    const testData38 = `obegcmqadtrhui
 qbgocuzeimrhdat
 icuagdbztxrqehoy
 cuietqhbfroagds
@@ -44,7 +18,77 @@ arke
 qzr
 plmgnr
 uriq`;
-  expect(countAnswers(parseData(testdata4))).toEqual(4);
-  expect(countAnswers(parseData(testdata5))).toEqual(5);
-  expect(countAnswers(parseData(testData38))).toEqual(38);
+    expect(parseDataPart2(testData38)).toEqual([
+      [
+        "obegcmqadtrhui",
+        "qbgocuzeimrhdat",
+        "icuagdbztxrqehoy",
+        "cuietqhbfroagds",
+        "uqdgikwhrvcjeltbpao",
+      ],
+      ["arke", "qzr", "plmgnr", "uriq"],
+    ]);
+  });
+
+  test("It finds matching letters for each entry in a group", () => {
+    const testData = ["abcxyz",
+"abc"];
+    expect(matchingAnswersPerGroup(testData)).toEqual(["a", "b", "c"]);
+  });
+
+  test("It finds matching letters for each entry in a group 2", () => {
+    const testData = ["acxyz",
+"abc"];
+    expect(matchingAnswersPerGroup(testData)).toEqual(["a", "c"]);
+  });
+});
+
+describe("part1", () => {
+  test("it parses the data correctly", () => {
+    const testdata = `ab
+    
+    c
+    d`;
+    expect(parseDataPart1(testdata)).toEqual([
+      ["a", "b"],
+      ["c", "d"],
+    ]);
+  });
+
+  test("it dedupes entries", () => {
+    const testdata = `ab
+    
+    cf
+    df`;
+
+    expect(parseDataPart1(testdata)).toEqual([
+      ["a", "b"],
+      ["c", "f", "d"],
+    ]);
+  });
+
+  test("it counts entries", () => {
+    const testdata4 = `ab
+    
+    a
+    b`;
+    const testdata5 = `ab
+      
+      cf
+      df`;
+
+    const testData38 = `obegcmqadtrhui
+qbgocuzeimrhdat
+icuagdbztxrqehoy
+cuietqhbfroagds
+uqdgikwhrvcjeltbpao
+
+arke
+qzr
+plmgnr
+uriq`;
+    expect(countAnswersPart1(parseDataPart1(testdata4))).toEqual(4);
+    expect(countAnswersPart1(parseDataPart1(testdata5))).toEqual(5);
+    expect(countAnswersPart1(parseDataPart1(testData38))).toEqual(38);
+  });
 });
