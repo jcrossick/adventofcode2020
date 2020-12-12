70c98faa35febb500b52933caf0cd41d5bc42ab5
Josephine Crossick <jocrossick@gmail.com>
2020-12-12 09:07:03 +0100

    Day 8 complete

--- day8.js
+++ day8.js
@@ -1,38 +1,66 @@
 const { input } = require("./day8data");
 const inputLines = input.split(/\n/);
 
-const getMoveAndAcc = (line) => {
-    const [instruction, value] = line.split(" ");
-    if (instruction == "nop") {
-      return [1, 0];
-    }
-  
-    if (instruction == "acc") {
-      return [1, parseInt(value)];
-    }
-  
-    if (instruction == "jmp") {
-      return [parseInt(value), 0];
-    }
-  
-    throw new Error("Did not receive nop/acc/jmp instruction");
-  };
+const getMoveAndChange = (line) => {
+  const [instruction, value] = line.split(" ");
+  if (instruction == "nop") {
+    return [1, 0];
+  }
 
-const runInput = (input) => {
+  if (instruction == "acc") {
+    return [1, parseInt(value)];
+  }
+
+  if (instruction == "jmp") {
+    return [parseInt(value), 0];
+  }
+
+  throw new Error("Did not receive nop/acc/jmp instruction");
+};
+
+const runInput = (input, replacementIndex, replacementInstruction) => {
   const indexRun = new Set();
   let indexVal = 0;
   let accumulator = 0;
 
-  while (!indexRun.has(indexVal)) {
+  while (indexVal < input.length) {
+    if (indexRun.has(indexVal)) {
+        indexVal = input.length;
+        break;
+    }
+
     indexRun.add(indexVal);
-    const [move, change] = getMoveAndAcc(input[indexVal]);
+    let [move, change] = getMoveAndChange(input[indexVal]);
+    if (replacementIndex == indexVal) {
+      [move, change] = getMoveAndChange(replacementInstruction);
+    }
     indexVal = indexVal + move;
     accumulator = accumulator + change;
+    if (indexVal > input.length -1) {
+        console.log("****************Reached the end of the run!!!!");
+    }
   }
-
-  return accumulator;
+  console.log("Returning from runInput", accumulator);
+  return [accumulator, indexRun];
 };
 
-console.log("part 1 is", runInput(inputLines));
+const tryDifferentRuns = (input) => {
 
-module.exports = { runInput, getMoveAndAcc };
+  const indexRun = runInput(input)[1];
+
+  indexRun.forEach(instructionIndex => {
+      if (input[instructionIndex].substring(0, 3) == 'jmp') {
+        runInput(input, instructionIndex, input[instructionIndex].replace('jmp', 'nop'));
+      }
+      if (input[instructionIndex].substring(0, 3) == 'nop') {
+        runInput(input, instructionIndex, input[instructionIndex].replace('nop', 'jmp'));
+    }
+  })
+};
+
+console.log("Part 2 is running", tryDifferentRuns(inputLines));
+
+//1317
+//console.log("part 1 is", runInput(inputLines)[0]);
+
+module.exports = { runInput, getMoveAndChange };
